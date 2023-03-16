0ef021e09d562a0be7ab3ce6bd04a71cc8df6df3
ademhoskin <ahoskin@gmu.edu>
2023-03-16 19:12:38 -0400

    more changes

--- converter.py
+++ converter.py
@@ -1,5 +1,5 @@
 # i will need pytube to get the audio mp4  and use ffmpeg to convert mp4 to mp3/wav, use regex to validate url
-from pytube import Youtube
+from pytube import YouTube
 import subprocess
 import re
 
@@ -20,7 +20,7 @@ class YoutubeConverter(object):
         self.url = url
         self.output_dir = output_dir
 
-    # uses the url and output_dir to validate and download audio in mp4 (i hate pytube)
+    # uses the url and output_dir to validate and download audio in mp4 (I hate pytube)
     def download_audio(self):
         # regex validation/exception implementation
         url_pattern = re.compile(r'https?://(www\.)?youtube\.com/watch\?v=\w+')
@@ -28,15 +28,30 @@ class YoutubeConverter(object):
             raise InvalidURL()
 
         # Download the YouTube video using pytube
-        yt = Youtube(self.url)
+        yt = YouTube(self.url)
         title = yt.title
         clean_title = re.sub('[^0-9a-zA-Z]+', '_', title)
         stream = yt.streams.filter(only_audio=True).first()
-        stream.download(output_path=self.output_dir, filename='audio')
+        stream.download(output_path=self.output_dir, filename=clean_title)
+
+        # returns clean title for conversion method
+        return clean_title
 
     # ffmpeg conversion method, will take the user's requested output format to convert the mp4 to (wav or mp3)
     def convert_audio(self, output_format):
         input_file = self.output_dir + ''
+        if output_format == 'mp3':
+            output_file = self.output_dir + /{clean_title}.mp3
+        elif output_format == 'wav':
+            output_file = self.output_dir + f'/{clean_title}.wav'
+        else:
+            print('Invalid output format!')
+            return
+
+
+        subprocess.call(command)
+
+
 
 
 
