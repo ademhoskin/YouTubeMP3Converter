83c4f9f148d90bad34d0d552bf374d774ecd4db0
ademhoskin <ahoskin@gmu.edu>
2023-03-17 13:17:54 -0400

    add gui functionally

--- __main__.py
+++ __main__.py
@@ -1,45 +1,72 @@
-# imports YoutuberConverter class, InvalidURL exception, os for directory validation and validate_url function
-
-import os
+import tkinter as tk
+from tkinter import filedialog
+import PySimpleGUI as sg
 from url_validation import validate_url
 from youtube_converter import YoutubeConverter
 from invalid_url import InvalidURL
 
-
-def main():
-    print("Welcome to Adem's YouTube to Audio Converter!")
-    # runs validation on url input
-    while True:
-        try:
-            url = input('Enter Youtube URl of video to convert: ')
-            if validate_url(url):
-                break
-        except InvalidURL as i:
-            print(i)
-    # runs validation on directory input
-    while True:
-        output_dir = input('Enter directory where you want the file to be saved to: ')
-        if os.path.isdir(output_dir):
-            break
-        else:
-            print('Invalid directory. Enter a valid directory path.')
-    # runs validation on output format
-    while True:
-        output_format = input('Enter output format (enter mp3 or wav): ')
-        if output_format == 'mp3' or output_format == 'wav':
-            break
-        else:
-            print('Invalid format. Enter mp3 or wav.')
-
-    # Uses class methods
+# Function to download audio from YouTube URL
+def download_audio(url, output_dir, output_format):
     try:
-        converter = YoutubeConverter(url, output_dir)
-        converter.download_audio(output_format)
-    # exception to cover unforeseen stuff
+        # Validate the URL
+        if validate_url(url):
+            # Download the audio using the YoutubeConverter class
+            converter = YoutubeConverter(url, output_dir)
+            converter.download_audio(output_format)
+            # Display success message using PySimpleGUI popup
+            sg.popup("Success", "Audio downloaded successfully!")
+        else:
+            # Raise an InvalidURL exception if URL is not valid
+            raise InvalidURL()
+    except InvalidURL as i:
+        # Display error message using PySimpleGUI popup
+        sg.popup_error("Error", str(i))
     except Exception as e:
-        print(e)
+        # Display error message using PySimpleGUI popup
+        sg.popup_error("Error", str(e))
 
+# Function to get output directory using a file dialog
+def get_output_directory():
+    root = tk.Tk()
+    root.withdraw()
+    # Open file dialog and get the selected directory
+    output_dir = filedialog.askdirectory()
+    root.destroy()
+    return output_dir
 
-# this is the main script so duh will run
-if __name__ == "__main__":
-    main()
+# PySimpleGUI layout for the main window
+layout = [
+    [sg.Text("YouTube URL:")],
+    [sg.Input(key="url")],
+    [sg.Text("Output directory:")],
+    [sg.Input(key="output_dir"), sg.Button("Browse", key="browse_output_dir")],
+    [sg.Text("Output format:")],
+    [sg.Radio("mp3", "format", default=True, key="mp3"), sg.Radio("wav", "format", key="wav")],
+    [sg.Button("Download Audio"), sg.Button("Exit")],
+]
+
+# Create PySimpleGUI window with the layout
+window = sg.Window("Adem's YouTube to Audio Converter", layout)
+
+# Event loop for the window
+while True:
+    event, values = window.read()
+
+    # Close the window if the "Exit" button or close icon is clicked
+    if event == sg.WIN_CLOSED or event == "Exit":
+        break
+
+    # Call the download_audio function if the "Download Audio" button is clicked
+    if event == "Download Audio":
+        url = values["url"]
+        output_dir = values["output_dir"]
+        output_format = "mp3" if values["mp3"] else "wav"
+        download_audio(url, output_dir, output_format)
+
+    # Call the get_output_directory function if the "Browse" button next to the output directory input is clicked
+    if event == "browse_output_dir":
+        output_dir = get_output_directory()
+        window["output_dir"].update(value=output_dir)
+
+# Close the window at the end of the event loop
+window.close()
