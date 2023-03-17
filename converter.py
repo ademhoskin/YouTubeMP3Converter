07d674dd6c2399e8d65f63e20bb3373bb21e7fc3
ademhoskin <ahoskin@gmu.edu>
2023-03-16 22:53:55 -0400

    Finalized classes

--- converter.py
+++ converter.py
@@ -31,23 +31,25 @@ class YoutubeConverter(object):
         # Download the YouTube video using pytube
         yt = YouTube(self.url)
         title = yt.title
-        clean_title = re.sub('[^0-9a-zA-Z]+', '_', title)
+        self.clean_title = re.sub('[^0-9a-zA-Z]+', '_', title)
         stream = yt.streams.filter(only_audio=True).first()
-        stream.download(output_path=self.output_dir, filename=clean_title)
+        stream.download(output_path=self.output_dir, filename=self.clean_title)
 
     # ffmpeg conversion method, will take the user's requested output format to convert the mp4 to (wav or mp3)
     def convert_audio(self, output_format):
         input_file = self.clean_title
         if output_format == 'mp3':
-            output_file = f'{self.output_dir}/{clean_title}.mp3'
+            output_file = f'{self.output_dir}/{self.clean_title}.mp3'
+            command = ["ffmpeg", "-i", input_file, "-vn", "-ar", "44100", "-ac", "2", "-b:a", "192k", output_file]
         elif output_format == 'wav':
-            output_file = self.output_dir + f'/{clean_title}.wav'
+            output_file = f'{self.output_dir}/{self.clean_title}.wav'
+            command = ["ffmpeg", "-i", input_file, "-vn", "-acodec", "pcm_s16le", "-ac", "2", "-ar", "44100", output_file]
         else:
             print('Invalid output format!')
             return
 
-
         subprocess.call(command)
+
 
 
 
